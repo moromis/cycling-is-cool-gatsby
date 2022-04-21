f1a44b71a3fd4d6ec7ffcfd3e6df36885846c25d
Kevin Ulrich <kzulrich@gmail.com>
2022-04-20 17:17:45 -0700

    fix stuff?

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -1,25 +1,25 @@
 /** @jsx jsx */
-import { Flex, jsx, useColorMode } from "theme-ui"
+import { Flex, jsx } from "theme-ui"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
-import ColorModeToggle from "./colormode-toggle"
+// import ColorModeToggle from "./colormode-toggle"
 import HeaderExternalLinks from "./header-external-links"
 import HeaderTitle from "./header-title"
 import Navigation from "./navigation"
 
 const Header = () => {
   const { navigation: nav } = useMinimalBlogConfig()
-  const [colorMode, setColorMode] = useColorMode()
-  const isDark = colorMode === `dark`
-  const toggleColorMode = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    setColorMode(isDark ? `light` : `dark`)
-  }
+  // const [colorMode, setColorMode] = useColorMode()
+  // const isDark = colorMode === `dark`
+  // const toggleColorMode = (e: React.SyntheticEvent) => {
+  //   e.preventDefault()
+  //   setColorMode(isDark ? `light` : `dark`)
+  // }
 
   return (
     <header sx={{ mb: [1, 2] }}>
       <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
         <HeaderTitle />
-        <ColorModeToggle isDark={isDark} toggle={toggleColorMode} />
+        {/* <ColorModeToggle isDark={isDark} toggle={toggleColorMode} /> */}
       </Flex>
       <div
         sx={{
